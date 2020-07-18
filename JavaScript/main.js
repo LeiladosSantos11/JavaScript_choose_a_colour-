628a7da11e91c7dc42f6322a062dfef7b71e9278
Leila <leiladossantos@yahoo.com>
2020-07-18 19:11:42 +0100

    Added the JS codes

---
+++ JavaScript/main.js
@@ -0,0 +1,60 @@
+const btnBoxColour01 = document.getElementById("colour_01");
+const btnBoxColour02 = document.getElementById("colour_02");
+const btnBoxColour03 = document.getElementById("colour_03");
+const btnBoxColour04 = document.getElementById("colour_04");
+const btnBoxColour05 = document.getElementById("colour_05");
+const btnBoxColour06 = document.getElementById("colour_06");
+const btnBoxColour07 = document.getElementById("colour_07");
+const btnBoxColour08 = document.getElementById("colour_08");
+const btnBoxColour09 = document.getElementById("colour_09");
+const btnBoxColour10 = document.getElementById("colour_10");
+const btnBoxColour11 = document.getElementById("colour_11");
+const btnBoxColour12 = document.getElementById("colour_12");
+
+colour_01.addEventListener("click", function () {
+  document.body.style.backgroundColor = "#ffc300";
+});
+
+colour_02.addEventListener("click", function () {
+  document.body.style.backgroundColor = "#7bc92d";
+});
+
+colour_03.addEventListener("click", function () {
+  document.body.style.backgroundColor = "#c92dc9";
+});
+
+colour_04.addEventListener("click", function () {
+  document.body.style.backgroundColor = "#18684e";
+});
+
+colour_05.addEventListener("click", function () {
+  document.body.style.backgroundColor = "#d3b3bd";
+});
+
+colour_06.addEventListener("click", function () {
+  document.body.style.backgroundColor = "#4d1c96";
+});
+
+colour_07.addEventListener("click", function () {
+  document.body.style.backgroundColor = "#c92d2d";
+});
+
+colour_08.addEventListener("click", function () {
+  document.body.style.backgroundColor = "#c92daf";
+});
+
+colour_09.addEventListener("click", function () {
+  document.body.style.backgroundColor = "#952dc9";
+});
+
+colour_10.addEventListener("click", function () {
+  document.body.style.backgroundColor = "#18409d";
+});
+
+colour_11.addEventListener("click", function () {
+  document.body.style.backgroundColor = "#1c5f96";
+});
+
+colour_12.addEventListener("click", function () {
+  document.body.style.backgroundColor = "#2d61c9";
+});
